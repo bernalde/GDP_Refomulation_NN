e2b378c6326cfc83f8035f370f4a3ca7b034e3d6
David Bernal Neira <bernalde@euler.cheme.cmu.edu>
2018-04-02 14:06:34 -0400

    completed experiments with k=10 and k=8

--- data/Logic_based_B_B_k10_i3_v80/Euler/parallel.sh
+++ data/Logic_based_B_B_k10_i3_v80/Euler/parallel.sh
@@ -10,10 +10,10 @@
 #PBS -l mem=5gb
 
 # Set name of job
-#PBS -N Job1
+#PBS -N k10_i3_v80_19
 
 # Use submission environment
 #PBS -V
-cd /home/bernalde/IntroToMLProj/GDP_Refomulation_NN/data/Logic_based_B_B_k10_i3_v50-80/Euler/19
+cd /home/bernalde/IntroToMLProj/GDP_Refomulation_NN/data/Logic_based_B_B_k10_i3_v80/Euler/19
 gams run_batch.gms
 
